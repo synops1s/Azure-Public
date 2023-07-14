679dd06e728a2b346de5d7eba508727614de1cf4
Douwstra <oscar.douwstra@fujitsu.com>
2023-07-14 16:13:45 +0200

    .

--- WinGet.ps1
+++ WinGet.ps1
@@ -2,6 +2,12 @@
 
 $TempPath = [System.IO.Path]::GetTempPath()
 
+$XamlUri = "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.0"
+$XamlFileName = "microsoft.ui.xaml.2.7"
+$XamlNugetFilePath = (Join-Path -Path $TempPath -ChildPath "$($XamlFileName).nupkg")
+$XamlZipFilePath = (Join-Path -Path $TempPath -ChildPath "$($XamlFileName).zip")
+$XamlAppxFilePath = (Join-Path -Path $TempPath -ChildPath "$($XamlFileName)\tools\AppX\x64\Release\$($XamlFileName).appx")
+
 $MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
 $MsixBundleFileName = $MsixBundleUri.Split("/")[-1]
 $MsixBundleFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleFileName)
@@ -10,13 +16,13 @@ $MsixBundleLicenseUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/m
 $MsixBundleLicenseFileName = $MsixBundleLicenseUri.Split("/")[-1]
 $MsixBundleLicenseFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleLicenseFileName)
 
-Write-Information "Downloading Winget to artifacts directory..."
+Invoke-WebRequest -Uri $XamlUri -OutFile $XamlNugetFilePath
 Invoke-WebRequest -Uri $MsixBundleUri -OutFile $MsixBundleFilePath
-
-Write-Information "Downloading WinGet License to artifacts directory..."
 Invoke-WebRequest -Uri $MsixBundleLicenseUri -OutFile $MsixBundleLicenseFilePath
 
-Add-ProvisionedAppxPackage -PackagePath $MsixBundleFilePath -LicensePath $MsixBundleLicenseFilePath -Online
+Remove-Item -Path $XamlZipFilePath -Force -ErrorAction SilentlyContinue
+Rename-Item -Path $XamlNugetFilePath -NewName $XamlZipFilePath
+Expand-Archive $XamlZipFilePath -Force
 
-Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
-Remove-Item -Path $MsixBundleLicenseFilePath -ErrorAction SilentlyContinue
+Add-ProvisionedAppxPackage -PackagePath $XamlAppxFilePath -Online -SkipLicense
+Add-ProvisionedAppxPackage -PackagePath $MsixBundleFilePath -LicensePath $MsixBundleLicenseFilePath -Online
