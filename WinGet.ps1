0b5f267cc8b2e7ed073e5caed5c2472a42017613
Douwstra <oscar.douwstra@fujitsu.com>
2023-07-14 11:21:37 +0200

    .

--- WinGet.ps1
+++ WinGet.ps1
@@ -1,10 +1,22 @@
-$WingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
-$WingetMsixBundle = $WingetMsixBundleUri.Split("/")[-1]
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-Write-Information "Downloading winget to artifacts directory..."
+$TempPath = [System.IO.Path]::GetTempPath()
 
-Invoke-WebRequest -Uri $WingetMsixBundleUri -OutFile "./$WingetMsixBundle"
-Invoke-WebRequest -Uri "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx" -OutFile "Microsoft.VCLibs.x64.14.00.Desktop.appx"
+$MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+$MsixBundleFileName = $MsixBundleUri.Split("/")[-1]
+$MsixBundleFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleFileName)
 
-Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage $latestWingetMsixBundle
+$VCLibsFileName = "Microsoft.VCLibs.x64.14.00.Desktop.appx"
+$VCLibsUri = "https://aka.ms/$($VCLibsFileName)"
+$VCLibsFilePath = (Join-Path -Path $TempPath -ChildPath $VCLibsFileName)
+
+Write-Information "Downloading VC Libs to artifacts directory..."
+Invoke-WebRequest -Uri $VCLibsUri -OutFile $VCLibsFilePath
+Write-Information "Downloading WinGet to artifacts directory..."
+Invoke-WebRequest -Uri $MsixBundleUri -OutFile $MsixBundleFilePath
+
+Add-AppxPackage -Path $VCLibsFilePath
+Add-AppxPackage -Path $MsixBundleFilePath
+
+Remove-Item -Path $VCLibsFilePath -ErrorAction SilentlyContinue
+Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
