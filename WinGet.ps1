6d2ca7f2960024fe82d003a2653822daca3b5414
Douwstra <oscar.douwstra@fujitsu.com>
2023-07-14 14:52:20 +0200

    .

--- WinGet.ps1
+++ WinGet.ps1
@@ -1,42 +1,24 @@
-# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# $TempPath = [System.IO.Path]::GetTempPath()
-
-# $MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
-# $MsixBundleFileName = $MsixBundleUri.Split("/")[-1]
-# $MsixBundleFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleFileName)
-
-# $VCLibsFileName = "Microsoft.VCLibs.x64.14.00.Desktop.appx"
-# $VCLibsUri = "https://aka.ms/$($VCLibsFileName)"
-# $VCLibsFilePath = (Join-Path -Path $TempPath -ChildPath $VCLibsFileName)
-
-# Write-Information "Downloading VC Libs to artifacts directory..."
-# Invoke-WebRequest -Uri $VCLibsUri -OutFile $VCLibsFilePath
-# Write-Information "Downloading WinGet to artifacts directory..."
-# Invoke-WebRequest -Uri $MsixBundleUri -OutFile $MsixBundleFilePath
-
-# Add-AppxPackage -Path $VCLibsFilePath
-# Add-AppxPackage -Path $MsixBundleFilePath
-
-# Remove-Item -Path $VCLibsFilePath -ErrorAction SilentlyContinue
-# Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 $TempPath = [System.IO.Path]::GetTempPath()
 
-$AppxProvisionedPackage = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq "Microsoft.DesktopAppInstaller" }
-if($AppxProvisionedPackage) {
-
-    Remove-AppxProvisionedPackage -Online -PackageName $AppxProvisionedPackage.PackageName
-}
-
-$MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+$MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object { $_.EndsWith(".msixbundle") }
 $MsixBundleFileName = $MsixBundleUri.Split("/")[-1]
 $MsixBundleFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleFileName)
 
-Write-Information "Downloading WinGet to artifacts directory..."
+$MsixBundleLicenseUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object { $_.EndsWith(".xml") }
+$MsixBundleLicenseFileName = $MsixBundleLicenseUri.Split("/")[-1]
+$MsixBundleLicenseFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleLicenseFileName)
+
+Write-Information "Downloading Winget to artifacts directory..."
 Invoke-WebRequest -Uri $MsixBundleUri -OutFile $MsixBundleFilePath
 
-Add-AppxProvisionedPackage -Online -PackagePath $MsixBundleFilePath -SkipLicense
+Write-Information "Downloading WinGet License to artifacts directory..."
+Invoke-WebRequest -Uri $MsixBundleLicenseUri -OutFile $MsixBundleLicenseFilePath
 
-# Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
+Add-AppxPackage -Path $MsixBundleFilePath -LicensePath $MsixBundleLicenseFilePath -AllUsers
+
+Add-ProvisionedAppxPackage -PackagePath $MsixBundleFilePath -LicensePath $MsixBundleLicenseFilePath -Online
+
+Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
+Remove-Item -Path $MsixBundleLicenseFilePath -ErrorAction SilentlyContinue
