a8f9e9c054354f71176a81e11c04c6b28b71dbd9
Douwstra <oscar.douwstra@fujitsu.com>
2023-07-14 13:21:16 +0200

    .

--- WinGet.ps1
+++ WinGet.ps1
@@ -24,24 +24,19 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $TempPath = [System.IO.Path]::GetTempPath()
 
-Write-Host "Checking if Winget is installed" -ForegroundColor Yellow
+$AppxProvisionedPackage = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq "Microsoft.DesktopAppInstaller" }
+if($AppxProvisionedPackage) {
 
-$TestWinget = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq "Microsoft.DesktopAppInstaller" }
-if ([Version]$TestWinGet.Version -gt "2022.506.16.0") {
-    
-    Write-Host "WinGet is Installed" -ForegroundColor Green
-} else {
-    
-    Write-Host "Not installed. Downloading WinGet..."
-
-    $MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
-    $MsixBundleFileName = $MsixBundleUri.Split("/")[-1]
-    $MsixBundleFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleFileName)
-
-    Write-Information "Downloading WinGet to artifacts directory..."
-    Invoke-WebRequest -Uri $MsixBundleUri -OutFile $MsixBundleFilePath
-
-    Add-AppxProvisionedPackage -Online -PackagePath $MsixBundleFilePath -SkipLicense
-
-    # Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
+    Remove-AppxProvisionedPackage -Online -PackageName $AppxProvisionedPackage.PackageName
 }
+
+$MsixBundleUri = $(Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/winget-cli/releases/latest").assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
+$MsixBundleFileName = $MsixBundleUri.Split("/")[-1]
+$MsixBundleFilePath = (Join-Path -Path $TempPath -ChildPath $MsixBundleFileName)
+
+Write-Information "Downloading WinGet to artifacts directory..."
+Invoke-WebRequest -Uri $MsixBundleUri -OutFile $MsixBundleFilePath
+
+Add-AppxProvisionedPackage -Online -PackagePath $MsixBundleFilePath -SkipLicense
+
+# Remove-Item -Path $MsixBundleFilePath -ErrorAction SilentlyContinue
